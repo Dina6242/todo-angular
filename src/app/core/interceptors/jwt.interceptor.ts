726efd68efcfd77763e72735b1f9c6b07d351958
dinasamer559@yahoo.com <dinasamer6242@gmail.com>
2020-12-01 21:25:38 +0200

    how to force user when token expire

--- src/app/core/interceptors/jwt.interceptor.ts
+++ src/app/core/interceptors/jwt.interceptor.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
+  constructor(private router: Router) {
+  }
+
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const token = localStorage.getItem('token');
     const req = request.clone({
@@ -11,6 +16,21 @@ export class JwtInterceptor implements HttpInterceptor {
         Authorization: `Bearer ${token}`,
       },
     });
-    return next.handle(req);
+    return next.handle(req).pipe(
+      catchError(
+        (err, caught) => {
+          if (err.status === 401) {
+            this.handleAuthError();
+            return of(err);
+          }
+          throw err;
+        },
+      ),
+    );
+  }
+
+  private handleAuthError(): void {
+    localStorage.removeItem('token');
+    this.router.navigateByUrl('login');
   }
 }
